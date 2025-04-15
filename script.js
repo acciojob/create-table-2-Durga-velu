3dfcefdbd56294bb7e24e0de8f193b31a75bc683
Durga-velu <durgadevi134babu@gmail.com>
2025-04-16 00:10:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function createTable() {
     //Write your code here
+let rowsCount=prompt("Input number of rows");
+	let columnsCount=prompt("Input number of columns")
+let table=document.getElementById("myTable")
+for(let i=0;i<rowsCount;i++){
+let rows=document.createElement("tr")
+	for(let j=0;j<columnsCount;j++){
+		let columns=document.createElement("td")
+		colums.textContent=`Row-${i} Column-${j}`
+	rows.appendChild(columns)	
+		
+	}
+
+	table.appendChild(rows)
+}
+	
+	
+	
+	
   
 }
