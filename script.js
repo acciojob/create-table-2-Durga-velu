d55c64d7cdbcfc0817cc4a1cbbbca1da4181c911
Durga-velu <durgadevi134babu@gmail.com>
2025-04-16 00:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ function createTable() {
     //Write your code here
 let rowsCount=prompt("Input number of rows");
 	let columnsCount=prompt("Input number of columns")
+
+
+	if(isNaN(rowsCount) || isNaN(columnsCount) || rowsCount<=0 || columnsCount <=0){
+		alert("Please enter the valid positive numbers.")
+		return;
+	}
+
+rowsCount=parseInt(rowsCount);
+	columnsCount=parseInt(columnsCount)
+	
 let table=document.getElementById("myTable")
+
+
+	table.innerHTML=""
 for(let i=0;i<rowsCount;i++){
 let rows=document.createElement("tr")
 	for(let j=0;j<columnsCount;j++){
